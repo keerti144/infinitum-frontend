2cc5383841be23bd43c40a4f01b2bae28e6ab950
DAKSHIN KARTHICK S <23z312@psgtech.ac.in>
2025-02-16 15:36:17 +0530

    changed form details and added scroll

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import FormField from "./FormField"
@@ -72,7 +72,7 @@ export default function RegisterPage() {
         <div className="absolute inset-0 bg-opacity-20 backdrop-blur-xl animate-blur"></div>
       </div>
 
-      <div className="relative z-10 bg-zinc-900 p-8 rounded-lg shadow-xl max-w-md w-full space-y-6 animate__animated animate__fadeIn mt-16 md:mt-24 md:max-w-sm">
+      <div className="relative z-10 bg-zinc-900 p-8 rounded-lg shadow-xl max-w-md w-full space-y-6 animate__animated animate__fadeIn mt-16 md:mt-24 md:max-w-sm overflow-y-auto max-h-screen custom-scrollbar">
         <h1 className="form-title text-4xl font-extrabold text-white text-center">
           Register for the Event
         </h1>
@@ -82,6 +82,12 @@ export default function RegisterPage() {
             <p>{message}</p>
           </div>
         )}
+
+        <button
+          className="form-button w-full mt-4 px-6 py-3 rounded-md bg-gray-800 text-white text-lg font-semibold hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-700 transition duration-300 scale-100 hover:scale-105 flex items-center justify-center"
+        >
+          Google Login
+        </button>
 
         <form className="space-y-4" onSubmit={handleSubmit}>
           <FormField label="Full Name" name="name" value={formData.name} handleChange={handleChange} placeholder="Enter your full name" />
@@ -123,7 +129,7 @@ export default function RegisterPage() {
 
           <button
             type="submit"
-            className="form-button w-full mt-4 px-6 py-3 rounded-md bg-[#fc1464] text-white text-lg font-semibold hover:bg-[#f41d72] focus:outline-none focus:ring-4 focus:ring-[#fc1464] transition duration-300 scale-100 hover:scale-105"
+            className="form-button w-full mt-4 px-8 py-4 rounded-md bg-[#fc1464] text-white text-lg font-semibold hover:bg-[#f41d72] focus:outline-none focus:ring-4 focus:ring-[#fc1464] transition duration-300 scale-100 hover:scale-105"
             disabled={loading}
           >
             {loading ? 'Registering...' : 'Register'}
