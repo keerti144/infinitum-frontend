943eed974fd1a82ebcbb2d8444487a38f4fec338
DAKSHIN KARTHICK S <23z312@psgtech.ac.in>
2025-03-03 22:03:11 +0530

    added 5th yr

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -216,6 +216,7 @@ export default function RegisterPage() {
                   <option value="2">2</option>
                   <option value="3">3</option>
                   <option value="4">4</option>
+                  <option value="5">5</option>
                 </select>
               </div>
 
