eb1abf854cd0cad5c0932253b28c27a6dbd008df
DARSHAN-V-G <darshanguna22@gmail.com>
2025-03-06 18:36:25 +0530

    change oauth authentication from supabase to default

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -28,9 +28,7 @@ export default function RegisterPage() {
   const [message, setMessage] = useState("");
   useEffect(() => {
     const token = localStorage.getItem("token");
-    if (token) {
-      router.push("/dashboard");
-    }
+    
 
     const storedName =
       searchParams.get("name") || localStorage.getItem("name") || "";
@@ -50,14 +48,17 @@ export default function RegisterPage() {
 
   const handleGoogleRegister = async () => {
     try {
-      const response = await fetch(`${BACKEND_URL}/api/auth/google`);
-      const data = await response.json();
+      const client_id = "415094605797-1a7k9df1k9atqltf47up2mgnf8lqdksk.apps.googleusercontent.com";
 
-      if (data.authUrl) {
-        window.location.href = data.authUrl;
-      } else {
-        console.error("Failed to get auth URL:", data.message);
-      }
+      const authUrl = `https://accounts.google.com/o/oauth2/auth?
+                          client_id=${client_id}&
+                          redirect_uri=https://infinitum.psgtech.ac.in/auth/callback&
+                          response_type=code&
+                          scope=email%20profile&
+                          access_type=offline&
+                          prompt=consent`;
+  
+      window.location.href = authUrl; // Redirect user to Google login
     } catch (error: unknown) {
       if (error instanceof Error) {
         console.error("Google Sign-in Error:", error.message);
@@ -66,7 +67,6 @@ export default function RegisterPage() {
       }
     }
   };
-
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
